7bd024fcf02c0e51bb2f36647ec8b1411e2fdb88
nadia <nadia0ostapyshyn@gmail.com>
2024-10-06 20:15:02 +0300

    refactor CompanyWidget for better readibllity

--- src/CompanyWidget.tsx
+++ src/CompanyWidget.tsx
@@ -24,6 +24,11 @@ interface CompanyWidgetProps {
   isLoading: boolean;
 }
 
+interface CompanyInfoViewProps {
+  companyInfo: CompanyType;
+  stockInfo: StockType;
+}
+
 const CompanyWidget = ({
   path,
   totalWindowCount,
@@ -36,9 +41,107 @@ const CompanyWidget = ({
   const [selectedTicker, setSelectedTicker] = useState<string | undefined>(
     undefined
   );
+
   const companyFullInfo = selectedTicker
     ? getCompanyInfoById(selectedTicker)
     : defaultTicker;
+
+  const CompanyInfoView = ({
+    companyInfo,
+    stockInfo,
+  }: CompanyInfoViewProps) => {
+    return (
+      <div>
+        <p>
+          <span className="font-bold">ticker </span>
+          <span>{companyInfo.ticker}</span>
+        </p>
+        <p>
+          <span className="font-bold">Name: </span>
+          <span>{companyInfo.name}</span>
+        </p>
+        <p>
+          <span className="font-bold">Legal name: </span>
+          <span>{companyInfo.legal_name}</span>
+        </p>
+        <p>
+          <span className="font-bold">Stock exchange: </span>
+          <span>{companyInfo.stock_exchange}</span>
+        </p>
+        <p>
+          <span className="font-bold">Short description: </span>
+          <span>{companyInfo.short_description}</span>
+        </p>
+        <p>
+          <span className="font-bold">Long description: </span>
+          <span>{companyInfo.long_description}</span>
+        </p>
+        <p>
+          <span className="font-bold">Web: </span>
+          <span>{companyInfo.company_url}</span>
+        </p>
+        <p>
+          <span className="font-bold">Business address: </span>
+          <span>{companyInfo.business_address}</span>
+        </p>
+        <br />
+        <p>
+          <span className="font-bold">Business phone: </span>
+          <span>{companyInfo.business_phone_no}</span>
+        </p>
+        <p>
+          <span className="font-bold">Entity legal form: </span>
+          <span>{companyInfo.entity_legal_form}</span>
+        </p>
+        <p>
+          <span className="font-bold">Latest filling date: </span>
+          <span>{companyInfo.latest_filing_date}</span>
+        </p>
+        <p>
+          <span className="font-bold">Inc country: </span>
+          <span>{companyInfo.inc_country}</span>
+        </p>
+        <p>
+          <span className="font-bold">Employees: </span>
+          <span>{companyInfo.employees}</span>
+        </p>
+        <p>
+          <span className="font-bold">Sector: </span>
+          <span>{companyInfo.sector}</span>
+        </p>
+        <p>
+          <span className="font-bold">Industry category: </span>
+          <span>{companyInfo.industry_category}</span>
+        </p>
+        <p>
+          <span className="font-bold">Industry group: </span>
+          <span>{companyInfo.industry_group}</span>
+        </p>
+        <p className="mb-0">
+          <span>First stock price date: </span>
+          <span>{stockInfo?.first_stock_price}</span>
+        </p>
+        <p>
+          <span>Last stock price date: </span>
+          <span>{stockInfo?.last_stock_price}</span>
+        </p>
+        <p>
+          <span className="font-bold">Thea enabled true legacy sector: </span>
+          <span>{companyInfo.legacy_sector}</span>
+        </p>
+        <p>
+          <span className="font-bold">Legacy industry category: </span>
+          <span>{companyInfo.legacy_industry_category}</span>
+        </p>
+        <p>
+          <span className="font-bold">Legacy industry group: </span>
+          <span>{companyInfo.legacy_industry_group}</span>
+        </p>
+      </div>
+    );
+  };
+
+  const { companyInfo, stockInfo } = companyFullInfo || {};
 
   return (
     <MosaicWindow<number>
@@ -79,98 +182,8 @@ const CompanyWidget = ({
           <div>Choose company from select</div>
         )}
         {isLoading && "Loading..."}
-        {!isLoading && companyFullInfo && companyFullInfo.companyInfo && (
-          <div>
-            <p>
-              <span className="font-bold">ticker </span>
-              <span>{companyFullInfo.companyInfo.ticker}</span>
-            </p>
-            <p>
-              <span className="font-bold">Name: </span>
-              <span>{companyFullInfo.companyInfo.name}</span>
-            </p>
-            <p>
-              <span className="font-bold">Legal name: </span>
-              <span>{companyFullInfo.companyInfo.legal_name}</span>
-            </p>
-            <p>
-              <span className="font-bold">Stock exchange: </span>
-              <span>{companyFullInfo.companyInfo.stock_exchange}</span>
-            </p>
-            <p>
-              <span className="font-bold">Short description: </span>
-              <span>{companyFullInfo.companyInfo.short_description}</span>
-            </p>
-            <p>
-              <span className="font-bold">Long description: </span>
-              <span>{companyFullInfo.companyInfo.long_description}</span>
-            </p>
-            <p>
-              <span className="font-bold">Web: </span>
-              <span>{companyFullInfo.companyInfo.company_url}</span>
-            </p>
-            <p>
-              <span className="font-bold">Business address: </span>
-              <span>{companyFullInfo.companyInfo.business_address}</span>
-            </p>
-            <br />
-            <p>
-              <span className="font-bold">Business phone: </span>
-              <span>{companyFullInfo.companyInfo.business_phone_no}</span>
-            </p>
-            <p>
-              <span className="font-bold">Entity legal form: </span>
-              <span>{companyFullInfo.companyInfo.entity_legal_form}</span>
-            </p>
-            <p>
-              <span className="font-bold">Latest filling date: </span>
-              <span>{companyFullInfo.companyInfo.latest_filing_date}</span>
-            </p>
-            <p>
-              <span className="font-bold">Inc country: </span>
-              <span>{companyFullInfo.companyInfo.inc_country}</span>
-            </p>
-            <p>
-              <span className="font-bold">Employees: </span>
-              <span>{companyFullInfo.companyInfo.employees}</span>
-            </p>
-            <p>
-              <span className="font-bold">Sector: </span>
-              <span>{companyFullInfo.companyInfo.sector}</span>
-            </p>
-            <p>
-              <span className="font-bold">Industry category: </span>
-              <span>{companyFullInfo.companyInfo.industry_category}</span>
-            </p>
-            <p>
-              <span className="font-bold">Industry group: </span>
-              <span>{companyFullInfo.companyInfo.industry_group}</span>
-            </p>
-            <p className="mb-0">
-              <span>First stock price date: </span>
-              <span>{companyFullInfo.stockInfo?.first_stock_price}</span>
-            </p>
-            <p>
-              <span>Last stock price date: </span>
-              <span>{companyFullInfo.stockInfo?.last_stock_price}</span>
-            </p>
-            <p>
-              <span className="font-bold">
-                Thea enabled true legacy sector:{" "}
-              </span>
-              <span>{companyFullInfo.companyInfo.legacy_sector}</span>
-            </p>
-            <p>
-              <span className="font-bold">Legacy industry category: </span>
-              <span>
-                {companyFullInfo.companyInfo.legacy_industry_category}
-              </span>
-            </p>
-            <p>
-              <span className="font-bold">Legacy industry group: </span>
-              <span>{companyFullInfo.companyInfo.legacy_industry_group}</span>
-            </p>
-          </div>
+        {!isLoading && companyFullInfo && companyInfo && stockInfo && (
+          <CompanyInfoView companyInfo={companyInfo} stockInfo={stockInfo} />
         )}
       </div>
     </MosaicWindow>
