27e39ed318210b0b5df972a70c03b0e0bdfd86c2
nadia <nadia0ostapyshyn@gmail.com>
2024-10-06 21:29:06 +0300

    move CompanyInfoView outside CompanyWidget

--- src/CompanyWidget.tsx
+++ src/CompanyWidget.tsx
@@ -27,6 +27,98 @@ interface CompanyInfoViewProps {
   stockInfo: StockType;
 }
 
+const CompanyInfoView = ({ companyInfo, stockInfo }: CompanyInfoViewProps) => {
+  return (
+    <div>
+      <p>
+        <span className="font-bold">ticker </span>
+        <span>{companyInfo.ticker}</span>
+      </p>
+      <p>
+        <span className="font-bold">Name: </span>
+        <span>{companyInfo.name}</span>
+      </p>
+      <p>
+        <span className="font-bold">Legal name: </span>
+        <span>{companyInfo.legal_name}</span>
+      </p>
+      <p>
+        <span className="font-bold">Stock exchange: </span>
+        <span>{companyInfo.stock_exchange}</span>
+      </p>
+      <p>
+        <span className="font-bold">Short description: </span>
+        <span>{companyInfo.short_description}</span>
+      </p>
+      <p>
+        <span className="font-bold">Long description: </span>
+        <span>{companyInfo.long_description}</span>
+      </p>
+      <p>
+        <span className="font-bold">Web: </span>
+        <span>{companyInfo.company_url}</span>
+      </p>
+      <p>
+        <span className="font-bold">Business address: </span>
+        <span>{companyInfo.business_address}</span>
+      </p>
+      <br />
+      <p>
+        <span className="font-bold">Business phone: </span>
+        <span>{companyInfo.business_phone_no}</span>
+      </p>
+      <p>
+        <span className="font-bold">Entity legal form: </span>
+        <span>{companyInfo.entity_legal_form}</span>
+      </p>
+      <p>
+        <span className="font-bold">Latest filling date: </span>
+        <span>{companyInfo.latest_filing_date}</span>
+      </p>
+      <p>
+        <span className="font-bold">Inc country: </span>
+        <span>{companyInfo.inc_country}</span>
+      </p>
+      <p>
+        <span className="font-bold">Employees: </span>
+        <span>{companyInfo.employees}</span>
+      </p>
+      <p>
+        <span className="font-bold">Sector: </span>
+        <span>{companyInfo.sector}</span>
+      </p>
+      <p>
+        <span className="font-bold">Industry category: </span>
+        <span>{companyInfo.industry_category}</span>
+      </p>
+      <p>
+        <span className="font-bold">Industry group: </span>
+        <span>{companyInfo.industry_group}</span>
+      </p>
+      <p className="mb-0">
+        <span>First stock price date: </span>
+        <span>{stockInfo?.first_stock_price}</span>
+      </p>
+      <p>
+        <span>Last stock price date: </span>
+        <span>{stockInfo?.last_stock_price}</span>
+      </p>
+      <p>
+        <span className="font-bold">Thea enabled true legacy sector: </span>
+        <span>{companyInfo.legacy_sector}</span>
+      </p>
+      <p>
+        <span className="font-bold">Legacy industry category: </span>
+        <span>{companyInfo.legacy_industry_category}</span>
+      </p>
+      <p>
+        <span className="font-bold">Legacy industry group: </span>
+        <span>{companyInfo.legacy_industry_group}</span>
+      </p>
+    </div>
+  );
+};
+
 const CompanyWidget = ({
   path,
   totalWindowCount,
@@ -41,101 +133,6 @@ const CompanyWidget = ({
   const companyFullInfo = selectedTicker
     ? getCompanyInfoById(selectedTicker)
     : defaultTicker;
-
-  const CompanyInfoView = ({
-    companyInfo,
-    stockInfo,
-  }: CompanyInfoViewProps) => {
-    return (
-      <div>
-        <p>
-          <span className="font-bold">ticker </span>
-          <span>{companyInfo.ticker}</span>
-        </p>
-        <p>
-          <span className="font-bold">Name: </span>
-          <span>{companyInfo.name}</span>
-        </p>
-        <p>
-          <span className="font-bold">Legal name: </span>
-          <span>{companyInfo.legal_name}</span>
-        </p>
-        <p>
-          <span className="font-bold">Stock exchange: </span>
-          <span>{companyInfo.stock_exchange}</span>
-        </p>
-        <p>
-          <span className="font-bold">Short description: </span>
-          <span>{companyInfo.short_description}</span>
-        </p>
-        <p>
-          <span className="font-bold">Long description: </span>
-          <span>{companyInfo.long_description}</span>
-        </p>
-        <p>
-          <span className="font-bold">Web: </span>
-          <span>{companyInfo.company_url}</span>
-        </p>
-        <p>
-          <span className="font-bold">Business address: </span>
-          <span>{companyInfo.business_address}</span>
-        </p>
-        <br />
-        <p>
-          <span className="font-bold">Business phone: </span>
-          <span>{companyInfo.business_phone_no}</span>
-        </p>
-        <p>
-          <span className="font-bold">Entity legal form: </span>
-          <span>{companyInfo.entity_legal_form}</span>
-        </p>
-        <p>
-          <span className="font-bold">Latest filling date: </span>
-          <span>{companyInfo.latest_filing_date}</span>
-        </p>
-        <p>
-          <span className="font-bold">Inc country: </span>
-          <span>{companyInfo.inc_country}</span>
-        </p>
-        <p>
-          <span className="font-bold">Employees: </span>
-          <span>{companyInfo.employees}</span>
-        </p>
-        <p>
-          <span className="font-bold">Sector: </span>
-          <span>{companyInfo.sector}</span>
-        </p>
-        <p>
-          <span className="font-bold">Industry category: </span>
-          <span>{companyInfo.industry_category}</span>
-        </p>
-        <p>
-          <span className="font-bold">Industry group: </span>
-          <span>{companyInfo.industry_group}</span>
-        </p>
-        <p className="mb-0">
-          <span>First stock price date: </span>
-          <span>{stockInfo?.first_stock_price}</span>
-        </p>
-        <p>
-          <span>Last stock price date: </span>
-          <span>{stockInfo?.last_stock_price}</span>
-        </p>
-        <p>
-          <span className="font-bold">Thea enabled true legacy sector: </span>
-          <span>{companyInfo.legacy_sector}</span>
-        </p>
-        <p>
-          <span className="font-bold">Legacy industry category: </span>
-          <span>{companyInfo.legacy_industry_category}</span>
-        </p>
-        <p>
-          <span className="font-bold">Legacy industry group: </span>
-          <span>{companyInfo.legacy_industry_group}</span>
-        </p>
-      </div>
-    );
-  };
 
   const { companyInfo, stockInfo } = companyFullInfo || {};
 
